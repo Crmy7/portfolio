9e9dc9862b10c6151d341bdd34339725fbf14f7e
Crmy7 <114735818+Crmy7@users.noreply.github.com>
2024-07-01 14:30:22 +0200

    Update Contact.vue

--- components/Contact.vue
+++ components/Contact.vue
@@ -4,7 +4,6 @@
             Contactez-moi
         </h3>
         <div class="iconContact">
-            <a href="mailto:charlesremy03@gmail.com" target="_blank" data-aos="fade-in" data-aos-offset="100" data-aos-delay="100" data-aos-duration="1000"><font-awesome-icon icon="fa-solid fa-envelope" class="icon"/></a>
             <a href="https://www.linkedin.com/in/charles-rémy/" target="_blank" data-aos="fade-in" data-aos-offset="100" data-aos-delay="100" data-aos-duration="1000"><font-awesome-icon icon="fa-brands fa-linkedin" class="icon"/></a>
             <a href="https://github.com/Crmy7" target="_blank" data-aos="fade-in" data-aos-offset="100" data-aos-delay="100" data-aos-duration="1000"><font-awesome-icon icon="fa-brands fa-github" class="icon"/></a>
         </div>
